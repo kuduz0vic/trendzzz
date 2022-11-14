996ef59ecb58327a7c1eed28c4a452354ac4b1e8
Arnes <arneskuduz3@gmail.com>
2022-11-15 00:52:47 +0100

    about us section

--- assets/theme.js
+++ assets/theme.js
@@ -25895,31 +25895,7 @@ function myFunction() {
  }
 }
 
-$(document).ready(function() {
-  // Get media - with autoplay disabled (audio or video)
-  var media = $('video').not("[autoplay='autoplay']");
-  var tolerancePixel = 40;
 
-  function checkMedia(){
-      // Get current browser top and bottom
-      var scrollTop = $(window).scrollTop() + tolerancePixel;
-      var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
-
-      media.each(function(index, el) {
-          var yTopMedia = $(this).offset().top;
-          var yBottomMedia = $(this).height() + yTopMedia;
-
-          if(scrollTop < yBottomMedia && scrollBottom > yTopMedia){ //view explaination in `In brief` section above
-              $(this).get(0).play();
-          } else {
-              $(this).get(0).pause();
-          }
-      });
-
-      //}
-  }
-  $(document).on('scroll', checkMedia);
-});
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.menu-icon3').forEach(trigger => {
@@ -25936,7 +25912,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-  
+
 
    swiper.slides[i].classList.add('remove-item');                                    
    swiper.slides[i].addEventListener('animationend', function () {
